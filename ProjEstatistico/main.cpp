f60ec4af6b771a9ee6463d3921725d5ed5c7cc4a
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-08-16 16:52:53 -0300

    .

---
+++ ProjEstatistico/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //biblioteca de entrada e saida de dados
+#include <math.h>   //biblioteca matematica
+using namespace std;
+
+int main()
+{
+    int tam;
+    cout <<"Quantos dados deseja registrar?"<<endl;
+    cin  << tam;
+    //system("cls");
+    int dados[tam];
+
+
+    return 0;
+}
+float KclassesCalculo(){
+
+}
+
