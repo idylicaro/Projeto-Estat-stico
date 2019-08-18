b8722c714f758abe9d83586e11c25dc203d6d3ab
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-08-17 23:02:28 -0300

    .

--- ProjEstatistico/main.cpp
+++ ProjEstatistico/main.cpp
@@ -2,18 +2,23 @@
 #include <math.h>   //biblioteca matematica
 using namespace std;
 
+float KclassesCalculo(int n);
 int main()
 {
     int tam;
     cout <<"Quantos dados deseja registrar?"<<endl;
-    cin  << tam;
+    cin  >> tam;
     //system("cls");
     int dados[tam];
+    int k = KclassesCalculo(tam)+1;  //entao como o tipo e int , ele retira tudo apos a virgula , ai adiciona 1 pq o autor do livro fala que e pra arrendondar pra cima
 
 
+    cout <<k;
     return 0;
 }
-float KclassesCalculo(){
-
+float KclassesCalculo(int n){
+    float k=0;
+    k = 1+(3.22*log10(n));
+    return k;
 }
 
