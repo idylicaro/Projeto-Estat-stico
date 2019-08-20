468d327db0c1c2429f8cefa66e2e2e82f200ec10
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-08-20 17:41:07 -0300

    agora começar a fazer a tabela

--- ProjEstatistico/func.h
+++ ProjEstatistico/func.h
@@ -26,3 +26,29 @@ int menor(int vet[],int TAM){
         return menor;
 }
 
+int verificaIntervalo(int k,int vet[],int tam,int h){
+    float menorr = menor(vet,tam);
+    float maiorr = maior(vet,tam);
+
+    menorr -=0.5;
+    maiorr -=0.5;
+    float intervaloTest = menorr;
+    int contador=0;
+    while(intervaloTest <= maiorr){
+        intervaloTest +=h;
+        contador++;
+    }
+        return contador;
+}
+
+void definindoIntervalos(int small,int k,int h,float intervalo[][2]){
+    float interval = small - 0.5;
+
+    for(int i = 0;i<k;i++){
+        for(int j =0;j<2;j++){
+            intervalo[i][j] = interval + (h*j);
+            interval += (h*j); // quando h for 0 vai ser o primeiro intervalo
+        }
+    }
+
+}
--- ProjEstatistico/main.cpp
+++ ProjEstatistico/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 #include "func.h"
-/* LEgenda de variaveis
+/* Legenda de variaveis
 * k  - Numero de classes
 * A  - Amplitude Total
 * h  - Amplitude total classe
@@ -17,7 +17,8 @@ using namespace std;
 */
 
 int main(void){
-    int A,k,h,f,pm,nf,NF,F;
+    int A,k,h,f,pm,nf,NF,F,small,biger;
+    //float intervalos[]
     int tam;
     cout <<"Quantos dados deseja registrar?"<<endl;
     cin  >> tam;
@@ -25,15 +26,33 @@ int main(void){
 
     int dados[tam];
     for(int i=0;i<tam;i++){
-        cout << "Digite o valor e pressione \"ENTER\"!"<<endl;
+        cout << i+1 << " Digite o valor e pressione \"ENTER\"!"<<endl;
         cin>>dados[i];
+        cout <<endl;
     }
+    small = menor(dados,tam);
+    biger = maior(dados,tam);
 
-    k = KclassesCalculo(tam)+1;  //entao como o tipo e int , ele retira tudo apos a virgula , ai adiciona 1 pq o autor do livro fala que e pra arrendondar pra cima
-    A = (maior(dados,tam))-(menor(dados,tam));
+    k = KclassesCalculo(50)+1;  //entao como o tipo e int , ele retira tudo apos a virgula , ai adiciona 1 pq o autor do livro fala que e pra arrendondar pra cima
+    A = (biger)-(small);
     h = A/k;
 
-    cout <<" "<<h;
+    k = verificaIntervalo(k,dados,tam,h);
+
+    float intervalos[k][2];
+    definindoIntervalos(small,k,h,intervalos);
+
+    //ler os intervalos
+    for(int i = 0;i<k;i++){
+        for(int j =0;j<2;j++){
+            cout <<intervalos[i][j]<< " ";
+
+        }
+        cout << "\n";
+        cout << "----------------------"<<endl;
+    }
+
+
     return 0;
 }
 
